30331a0d2930c09392d3e7b6271c1f9d8da65a09
Sebastian Kalicki <sebastian@kalicki.email>
2017-05-23 05:54:38 +0200

    Finishing auto complete, but without style.

--- src/auto-complete.component.ts
+++ src/auto-complete.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'base-auto-complete',
@@ -8,11 +8,69 @@ import { Component, Input, OnInit } from '@angular/core';
 export class AutoCompleteComponent implements OnInit {
 
   @Input() source: Array<Object>;
-  @Input('search-query') searchQuery: string;
+  @Input() query: string;
+  @Input() input: HTMLElement;
 
-  constructor () { }
+  @Output() value: EventEmitter<Object> = new EventEmitter();
+
+  private filteredSource: Array<Object>;
+  private autoCompleteMenuItems: Array<HTMLElement>;
+  private numberOfMatches: number = 0;
+  private selectedIndex: number = -1;
+
+  constructor (private elementRef: ElementRef) {}
 
   ngOnInit () {
+  }
+
+  ngAfterViewChecked () {
+    this.autoCompleteMenuItems = this.elementRef.nativeElement.querySelectorAll('li');
+  }
+
+  onKeyDown ($event): void {
+    const KEY = {
+      ESC: 27,
+      UP: 38,
+      DOWN: 40,
+      ENTER: 13,
+      TAB: 9
+    };
+
+    switch ($event.keyCode) {
+      case KEY.UP:
+        this.selectedIndex = this.selectedIndex === -1 || this.selectedIndex === 0 ? this.numberOfMatches : --this.selectedIndex;
+        this.selectAutoCompleteMenuItem();
+        $event.preventDefault();
+        break;
+      case KEY.DOWN:
+        this.selectedIndex = this.selectedIndex === this.numberOfMatches ? 0 : ++this.selectedIndex;
+        this.selectAutoCompleteMenuItem();
+        $event.preventDefault();
+        break;
+      case KEY.ENTER:
+      case KEY.TAB:
+        this.confirmSelectedAutoCompleteMenuItem();
+        break;
+    }
+  }
+
+  public autoCompleteFilter (query: string): Array<Object> {
+    let result = this.source.filter(item => Object.keys(item)
+        .filter(key => item[key] !== null)
+        .some(key => (item[key].toString().toLowerCase()).indexOf(query.toLowerCase()) !== -1));
+
+    this.numberOfMatches = result.length - 1;
+    return this.filteredSource = result;
+  }
+
+  private selectAutoCompleteMenuItem (): void {
+    this.autoCompleteMenuItems.forEach((item, index) => {
+      this.selectedIndex === index ? item.classList.add('selected') : item.classList.remove('selected');
+    });
+  }
+
+  private confirmSelectedAutoCompleteMenuItem (): void {
+    this.value.emit(this.filteredSource[this.selectedIndex]);
   }
 
 }
