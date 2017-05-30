f30bcb985cb6ef0e45b79debf96ff587eea90ee4
Sebastian Kalicki <s.kalicki@yieldlab.de>
2017-05-30 19:24:05 +0200

    Adding index.ts in view of npm packages.

---
+++ index.ts
@@ -0,0 +1,9 @@
+import {BaseAutoCompleteModule} from './src/base-auto-complete.module';
+import {BaseAutoCompleteComponent} from './src/base-auto-complete.component';
+import {BaseAutoCompleteDirective} from './src/base-auto-complete.directive';
+
+export {
+  BaseAutoCompleteModule,
+  BaseAutoCompleteComponent,
+  BaseAutoCompleteDirective
+}
